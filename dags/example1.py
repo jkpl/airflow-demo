4d985bc6743947e6d3e75274ca99c2f90dd335b4
Jaakko Pallari <jkpl@lepovirta.org>
2020-05-13 16:02:01 +0300

    update dags

--- dags/example1.py
+++ dags/example1.py
@@ -1,6 +1,12 @@
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 
 
+dag = DAG(
+    'tutorial1',
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+)
+
 k = KubernetesPodOperator(
     namespace="default",
     image="debian:10-slim",
@@ -11,5 +17,5 @@ k = KubernetesPodOperator(
     task_id="task",
     is_delete_operator_pod=True,
     hostnetwork=False,
+    dag=dag,
 )
-
